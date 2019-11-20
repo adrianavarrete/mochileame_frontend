024a0e0a55c23496108a828f489aac36b0f7a187
Adria <adri.navarrete.09@gmail.com>
2019-11-20 17:56:44 +0100

    Arreglo bug 'details'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,10 +12,6 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   },
-  {
-    path: 'details',
-    loadChildren: () => import('./pages/details/details.module').then( m => m.DetailsPageModule)
-  },
 
 ];
 
