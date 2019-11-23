73eab38b57f2c1da0fdea2e21a06f0e723eb26f3
Adria <adri.navarrete.09@gmail.com>
2019-11-23 17:16:49 +0100

    Ya se puede completar un perfil + aplicado los services

--- src/app/services/travel-group.service.ts
+++ src/app/services/travel-group.service.ts
@@ -5,5 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class TravelGroupService {
 
+  readonly URL_API = 'http://localhost:3000/travelgroup';
+
+
   constructor() { }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  readonly URL_API = 'http://localhost:3000/user';
+  selectedUser: User;
+  users: User[];
+
+
+  constructor(private http: HttpClient) { }
+
+  updateUser(user: User) {
+    console.log(user);
+    return this.http.put(this.URL_API + `/updateUser/${user._id}`, user);
+
+  }
 }
+
+
