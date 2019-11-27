9e79881d691858ccaf70c4f9e711274cdde5bec7
Guillermo Manterola Rojas <guillermo.manterola@raona.com>
2019-11-27 19:18:43 +0100

    asqkeroso

--- src/app/pages/loguin/loguin.page.ts
+++ src/app/pages/loguin/loguin.page.ts
@@ -5,12 +5,8 @@ import { NgForm } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
-<<<<<<< HEAD
-import {Router} from '@angular/router';
-=======
 import { Router} from "@angular/router";
 
->>>>>>> 0c6d73e2dc41629e6c4cf032a5283879c43929c6
 
 
 
@@ -23,11 +19,7 @@ import { Router} from "@angular/router";
 export class LoguinPage implements OnInit {
 
 
-<<<<<<< HEAD
-  constructor(private router: Router,private userService: UserService) { }
-=======
   constructor(private userService: UserService, private router: Router) { }
->>>>>>> 0c6d73e2dc41629e6c4cf032a5283879c43929c6
   userLogin: User;
 
   ngOnInit() {
