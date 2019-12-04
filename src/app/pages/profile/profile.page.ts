0a935b005815180714acd8666d918bd969368ae6
Adrià Navarrete Pardo <adri.navarrete.09@gmail.com>
2019-12-04 23:44:54 +0100

    Seguir usuarios mejorado

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/models/user';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -10,15 +11,21 @@ import { User } from 'src/app/models/user';
 export class ProfilePage implements OnInit {
 
   user = new User();
-  myUser = new User();
+  myUser = new User('', '', '', '', '', '', '', '', '', '', '', '', '', '');
+
   username = localStorage.getItem('usernameToFollow');
   following: boolean;
 
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private route: ActivatedRoute) { 
+    route.params.subscribe(val => { // necesario para poder volver a ejecutar ngoninit al volver de otra pagina
+      this.getUsuarioByUsername(this.username);
+      this.checkFollowing(localStorage.getItem('idUser'));
+    });
+  }
 
   ngOnInit() {
-    this.getUserByUsername(this.username);
+    this.getUsuarioByUsername(this.username);
     this.checkFollowing(localStorage.getItem('idUser'));
 
   }
@@ -35,42 +42,69 @@ export class ProfilePage implements OnInit {
   }
 
   checkFollowing(myId: string) {
+    this.following = false;
     this.userService.getUsuario(myId)
       .subscribe(res => {
         console.log(res);
-        this.myUser = res;
-        console.log(this.user);
-
-      });
-
-    console.log(this.myUser.following);
-
-    if (this.myUser.following === null) {
-      this.following = false;
-    } else {
-      this.myUser.following.forEach(element => {
-        if (element === this.user._id) {
-          this.following = true;
-        } else {
+        if (res.following === null) {
           this.following = false;
+        } else {
+          res.following.forEach(element => {
+            console.log(element);
+            console.log(this.user._id);
+            if (element === this.user._id) {
+              this.following = true;
+              console.log(this.following);
+            }
+          });
         }
+        this.myUser = res;
       });
-    }
-
     console.log(this.following);
 
   }
 
-  getUserByUsername() {
-    this.userService.getUserByUsername(this.username)
+  checkFollowing2(myId: string) {
+
+    this.following = false;
+    this.userService.getUsuario(myId)
+      .subscribe(res => {
+        console.log(res);
+        if (res.following === null) {
+          this.following = false;
+        } else {
+          for (let element of res.following) {
+            console.log(element);
+            if (element === this.user._id) {
+              this.following = true;
+              console.log(this.following);
+              break;
+            }
+          }
+        }
+
+        this.myUser = res;
+      });
+    console.log(this.following);
+
+  }
+
+  getUsuarioByUsername(username: string) {
+    this.userService.getUserByUsername(username)
       .subscribe(res => {
         console.log(this.username);
         console.log(res);
         this.user = res;
-      })
+      });
   }
 
-  follow() {
+  follow(idToFollow: string) {
+    this.myUser.following.push(idToFollow);
+    this.userService.updateUser(this.myUser)
+      .subscribe(res => {
+        console.log(res);
+        this.myUser = res;
+      });
 
   }
 
